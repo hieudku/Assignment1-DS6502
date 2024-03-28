44d9ab440870a59c4b5f8f0bbadcf2bc39de5446
Huba <hieu.dcu@gmail.com>
2024-03-29 01:41:55 +1300

    Updated RScript.R
    install + import ggplot2 & dplyr
    imported file as dataset.df
    test imported file - worked on my PC

--- RScript.R
+++ RScript.R
@@ -14,4 +14,28 @@
 
 # frances branch new
 
+################################################################################
+
+# Dont worry if you overwrite these codes by accident when pushing main, 
+# I ll keep them on a backup branch as we go (Hieu)
+
+
+getwd()
+
+# do we even need to set wd lol? maybe not if it works on your R studio? (Hieu)
+
+# Install package - to be added more if needed
+install.packages("ggplot2")
+
+# Import package - to be added more if needed
+library(ggplot2)
+library(dplyr)
+
+# Import dataset
+dataset.df <- read.csv("./Excel File/TimeOnSocialMedia.csv")
+
+# Test a few first line of imported file
+head(dataset.df)
+tail(dataset.df)
+
 
