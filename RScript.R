6f279254c38dfdda079817dd93a0908bbee37a78
AdyaS <1810adya@gmail.com>
2024-03-30 21:38:46 +1300

    heatmap and histogram

--- RScript.R
+++ RScript.R
@@ -1,13 +1,43 @@
 
+######################################## 
+#### Adya Q4- heatmaps ####
 
-# this is frances's branch, the latest version
-# adya
-#pls work
+# Installing the required package
+install.packages("reshape2")
 
-#qwerty asdfgh zxcvb
+# Loading from the library
+library(reshape2)
 
-#############
-#adya new again#
+# Preparing the dataset
+dataset.melt <- melt(dataset.df)
 
-# frances branch new
+# Calculating average time spent by gender
+avg_time_spent <- aggregate(time_spent ~ gender, dataset.df, mean)
 
+# Convert the data to long format for the heatmap
+avg_time_spent_long <- reshape2::melt(avg_time_spent, id.vars = "gender")
+
+# Creating a heatmap
+ggplot(avg_time_spent_long, aes(x = gender, y = variable, fill = value)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient(low = "yellow", high = "blue") +
+  labs(title = "Heatmap of Average Time Spent by Gender",
+       x = "Gender",
+       y = "Time Spent",
+       fill = "Average Time Spent")
+
+# Creating a histogram for each numerical column in the dataset
+for(col in names(dataset.df)) {
+  # Check if the column is numerical
+  if(is.numeric(dataset.df[[col]])) {
+    # Create a histogram
+    p <- ggplot(dataset.df, aes_string(col)) + 
+      geom_histogram(binwidth = 10, fill = "green", color = "black") +
+      labs(title = paste("Histogram of", col),
+           x = col,
+           y = "Frequency")
+    print(p)
+  }
+}
+
+######################################
